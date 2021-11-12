1ba2d9614ccfb68506994095f6f9daaed6f16d1f
777shetty <shetty.chetu7@gmail.com>
2021-11-12 09:27:19 +0530

    MODIFIED:Peopwer files followed

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
-import PayrollForm from './components/payroll-form/payroll-form';
+import PayrollForm from './components/payroll-form/PayrollForm';
 import {
   BrowserRouter as Router,
   Switch, 
-    Route , Redirect
+    Route , 
+    Redirect,
   }from "react-router-dom";
 function App() {
   return (
     <div className="App">
       <Router>
         <Switch>
-          <Route extra path="/payroll-form">
-            <PayrollForm />
-          </Route>
+        <Route path="/" component={PayrollForm} exact />
         </Switch>
       </Router>
     </div>
