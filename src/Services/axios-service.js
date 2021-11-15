26c36149a1f5f2e78fae2e07ffa7cb7e26ff3880
777shetty <shetty.chetu7@gmail.com>
2021-11-15 20:42:08 +0530

    ADDED:ability to add service

--- src/Services/axios-service.js
+++ src/Services/axios-service.js
@@ -1,13 +1,29 @@
 const axios = require('axios').default;
 
 class AxiosService {
-    postService(url = '', payload = null, tokenRequired = false, httpOptions = null) {
+    postService(
+        url = "",
+        payload = null,
+        tokenRequired = false,
+        httpOptions = null
+      ) {
         return axios.post(url, payload, tokenRequired && httpOptions);
-    }
-
-    getService(url = '', tokenRequired = false, httpOptions = null) {
+      }
+      putService(
+        url = "",
+        payload = null,
+        tokenRequired = false,
+        httpOptions = null
+      ) {
+        return axios.put(url, payload, tokenRequired && httpOptions);
+      }
+    
+      deleteService(url = "", tokenRequired = false, httpOptions = null) {
+        return axios.delete(url, tokenRequired && httpOptions);
+      }
+    
+      getService(url = "", tokenRequired = false, httpOptions = null) {
         return axios.get(url, tokenRequired && httpOptions);
+      }
     }
-}
-
 module.exports = new AxiosService()
